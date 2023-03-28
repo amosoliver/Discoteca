f5a2de046fef72b70e53bda61fd964a5b243a23c
amosoliver <oliveramos13542@gmail.com>
2023-03-28 19:46:40 -0400

    cancelado

--- resources/views/artista/index.blade.php
+++ resources/views/artista/index.blade.php
@@ -55,60 +55,14 @@
                                 {{$art->historia}}
                             </td>
                             <td>
-                                <button type="button" data-toggle="modal" data-target="#myModal" data-id="{{$art->id_artista}}"> EDITAR</button>
+                                <button type="button" onclick="window.location='{{ route('artista.edit',$art->id_artista) }}'">
+                                    EDITAR
+                                </button>
                             </td>
                     @endforeach
                     </tbody>
                 </table>
             </div>
-        </div>
-    </div>
-</div>
-<script>
-    $('#myModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget);
-        var idArtista = button.data('id');
-        $('#genre-id').text(idArtista);
-        $('#id_artista').val(idArtista);
-    });
-
-</script>
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">O que deseja ver do genero <span id="genre-id"></span></h4>
-            </div>
-            <div class="modal-body">
-                <div class="form-group">
-                    {!! Form::model($artista, ['method' => 'PATCH', 'route' => ['artista.update', $idArtista], 'class' => 'form-horizontal']) !!}
-                    {{ Form::label('ds_artista', 'Nome') }}
-                    <div class="col-md-7 col-lg-7">
-                        {{ Form::text('ds_artista',$artista->ds_artista , ['autofocus']) }}
-                    </div>
-                </div>
-                <div class="form-group">
-                    {{ Form::label('historia', 'Historia') }}
-                    <div class="col-md-7 col-lg-7">
-                        {{ Form::text('historia',$artista->historia , ['autofocus']) }}
-                    </div>
-                </div>
-                <div class="form-group">
-                    {{ Form::label('id_genero', 'Genero', ['class' => 'control-label col-md-3 col-lg-2']) }}
-                    <div class="col-md-7 col-lg-7">
-                        {{ Form::select('id_genero',$generos)}}
-                        {!! $errors->first('id_genero')!!}
-                    </div>
-                </div>
-                {{ Form::hidden('id_artista', null, ['id' => 'id_artista']) }}
-                <br>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
-                <button type="submit" class="btn btn-primary">Salvar mudanças</button>
-            </div>
-            {!! Form::close() !!}
         </div>
     </div>
 </div>
