bdae722dab6007ec16666263c805b4274b625894
amosoliver <oliveramos13542@gmail.com>
2023-04-27 21:30:41 -0400

    wip

--- resources/views/artista/index.blade.php
+++ resources/views/artista/index.blade.php
@@ -10,26 +10,26 @@
 
 @include('layout.navbar')
 
-    <div class="container">
-        <div class="row">
-    <div class="col-sm-12">
-        <div class="card-deck d-flex flex-wrap justify-content-center">
-            @foreach($artista as $art)
-                <div class="card text-dark col-sm-3 mb-3 mb-4">
-                    <img class="card-img-top img-fluid" src="{{ asset($art->imagem) }}" alt="Imagem do Card">
-                    <div class="card-body">
-                        <h5 class="card-title">{{$art->ds_artista}}</h5>
-                        <p class="card-text">{{$art->historia}}</p>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card-deck d-flex flex-wrap justify-content-center">
+                @foreach($artista as $art)
+                    <div class="card text-dark col-md-12 col-lg-3 mb-3 mb-md-4 mx-3">
+                        <img class="card-img-top img-fluid" src="{{ asset($art->imagem) }}" alt="Imagem do Card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$art->ds_artista}}</h5>
+                            <p class="card-text">{{$art->historia}}</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
                     </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
-    </div>
-    </div>
+</div>
 
 
 
