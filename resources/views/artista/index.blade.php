32cff1c6547e87b8e723bdf2c1c03759a34f0bbe
amosoliver <oliveramos13542@gmail.com>
2023-05-02 20:14:36 -0400

    chega

--- resources/views/artista/index.blade.php
+++ resources/views/artista/index.blade.php
@@ -12,24 +12,24 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-lg-12">
-            <div class="card-deck d-flex flex-wrap justify-content-center">
-                @foreach($artista as $art)
-                    <div class="card text-dark col-md-12 col-lg-3 mb-3 mb-md-4 mx-3">
-                        <img class="card-img-top img-fluid" src="{{ asset($art->imagem) }}" alt="Imagem do Card">
-                        <div class="card-body">
-                            <h5 class="card-title">{{$art->ds_artista}}</h5>
-                            <p class="card-text">{{$art->historia}}</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Last updated 3 mins ago</small>
-                        </div>
+        @foreach($artista as $art)
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+                <div class="card text-dark">
+                    <img class="card-img-top img-fluid" src="{{ asset($art->imagem) }}" alt="Imagem do Card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$art->ds_artista}}</h5>
+                        <p class="card-text">{{$art->historia}}</p>
+                        <a href="{{ route('artista.show', $art->id_artista) }}" class="btn btn-primary">VER ARTISTA</a>
                     </div>
-                @endforeach
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
             </div>
-        </div>
+        @endforeach
     </div>
 </div>
+
 
 
 
