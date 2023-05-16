b72d483c9510adf633bc1ee2495dee8711fb571b
amosoliver <oliveramos13542@gmail.com>
2023-05-15 20:15:23 -0400

    wip

--- resources/views/layout/welcome.blade.php
+++ resources/views/layout/welcome.blade.php
@@ -1,25 +1,4 @@
-@extends('layout.css')
-<section class="vh-100 gradient-custom">
-    <link rel="stylesheet" href="/public/css/style.css">
-    <div class="bg-secondary bg-gradient">
-    <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                <div class="card bg-primary text-white" >
-                    <div class="card-body p-5 text-center">
-                        <button type="button" onclick="window.location='{{ route('artista.index')}}'">
-                            Artistas
-                        </button>
-                        <button type="button" onclick="window.location='{{ route('disco.index')}}'">
-                            Discos
-                        </button>
-                        <button type="button" onclick="window.location='{{ route('genero.index')}}'">
-                            Gêneros
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    </div>
-</section>
+@extends('layout.default')
+@section('main')
+
+@endsection
