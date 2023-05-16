7fbfeb9c5868253896f33b1c1f77fa119d3eab50
amosoliver <oliveramos13542@gmail.com>
2023-05-16 12:57:13 -0400

    wip

--- database/seeders/GeneroTableSeeder.php
+++ database/seeders/GeneroTableSeeder.php
@@ -9,7 +9,9 @@ class GeneroTableSeeder extends Seeder
 {
     public function run()
     {
-        $generos = ['Gospel', 'Sertanejo', 'Rock', 'Axé', 'Metal', 'Jazz', 'Samba', 'Blues'];
+        $generos = ['Gospel', 'Sertanejo', 'Rock', 'Axé', 'Metal', 'Jazz', 'Samba', 'Blues', 'Eletrônica', 'Forró',
+        'Jazz', 'MPB', 'Pagode', 'Reggae', 'Black Music', 'Classic Rock', 'Disco', 'Folk', 'Grunge', 'Hard Rock',
+        'Indie', 'Progressivo', 'Ska', 'Soft Rock'];
 
         foreach ($generos as $genero) {
             Genero::create([
